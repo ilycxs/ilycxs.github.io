1e3d30c4cfb1dad40ebd0a5fd6821ec22deb4413
Chloe Sofia Encenzo <encenzochloe@gmail.com>
2021-01-13 06:17:57 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+setTimeout(function(){
+    $("#loading").addClass("animated fadeOut");
+    setTimeout(function(){
+      $("#loading").removeClass("animated fadeOut");
+      $("#loading").css("display","none");
+    },800);
+},1650);
+
+$(document).ready(function( ){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('body,html').animate({scrollTop: $(hash).offset().top}, 1800, function(){
+            window.location.hash = hash;
+       });
+       } 
+      });
+  });
+  
